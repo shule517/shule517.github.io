c3abf17582172a803d730e9c2b3f9c1831358d68
shule517 <shule517@gmail.com>
2016-02-07 16:23:02 +0900

    /lib/配下にjs,cssをコピーするように修正

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,19 @@ module.exports = function (grunt) {
       install: {
         options: {
           targetDir: './lib', //ライブラリの配置先のディレクトリ
-          layout: 'byType', // レイアウト、説明は後述します
           install: true, //grunt実行時にbower installを実行するかどうか
           verbose: false, // ログの詳細を出すかどうか
           cleanTargetDir: true, // targetDirを削除するかどうか
-          cleanBowerDir: false // bowerのcomponentsディレクトリを削除するかどうか
+          cleanBowerDir: false, // bowerのcomponentsディレクトリを削除するかどうか
+          layout : function (type, component) {
+            if (type === 'css') {
+              return 'css';
+            } else if (type === 'js') {
+              return 'js';
+            } else {
+               return 'fonts';
+            }
+          }
         }
       }
     },
